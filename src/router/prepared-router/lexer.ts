11f6f244be389567f78f925658155f987e3597b5
Ame_x Edam <edame8080@gmail.com>
2024-11-12 08:50:10 +0000

    wip

---
+++ src/router/prepared-router/lexer.ts
@@ -0,0 +1,150 @@
+type PathSeparator = {
+    type: 'separator',
+    value: '/'
+}
+
+type PathStatic = {
+    type: 'static',
+    value: string
+}
+
+type PathDynamic = {
+    type: 'dynamic',
+    value: string,
+    regex: string
+}
+
+type PathAlways = {
+    type: 'always',
+    value: '*'
+}
+
+type PathElement = PathSeparator | PathStatic | PathDynamic | PathAlways
+
+export type PathTree = PathElement[]
+
+export const pathLexer = (path: string): PathTree => {
+    const reader = (function* () {
+        while (path.length) {
+            yield path[0]
+            path = path.slice(1)
+        }
+
+        return null
+    })()
+
+    const pathTree: PathTree = []
+
+    const staticPathLexer = (staticPath: string) => {
+
+        while (true) {
+            const char = reader.next().value
+            if (!char) {
+                pathTree.push({
+                    type: 'static',
+                    value: staticPath
+                })
+
+                break
+            }
+
+            if (char === '/') {
+                pathTree.push({
+                    type: 'static',
+                    value: staticPath
+                })
+
+                pathTree.push({
+                    type: 'separator',
+                    value: '/'
+                })
+
+                break
+            }
+
+            staticPath += char
+        }        
+    }
+
+    const dynamicPathLexer = () => {
+        let paramName = ''
+        let regex = ''
+
+        const dynamicPathRegexLexer = () => {
+            while (true) {
+                const char = reader.next().value
+
+                if (!char) {
+                    break
+                }
+
+                if (char === '}') {
+                    break
+                }
+
+                regex += char
+            }
+        }
+
+        while (true) {
+            const char = reader.next().value
+
+            if (!char) {
+                pathTree.push({
+                    type: 'dynamic',
+                    value: paramName,
+                    regex
+                })
+
+                break
+            }
+
+            if (char === '/') {
+                pathTree.push({
+                    type: 'dynamic',
+                    value: paramName,
+                    regex
+                })
+
+                pathTree.push({
+                    type: 'separator',
+                    value: '/'
+                })
+
+                break
+            }
+
+            if (char === '{') {
+                dynamicPathRegexLexer()
+                continue
+            }
+            
+            paramName += char
+        }
+    }
+
+    for (const char of reader) {
+        switch (char) {
+            case '/':
+                pathTree.push({
+                    type: 'separator',
+                    value: char
+                })
+                continue
+            case ':':
+                dynamicPathLexer()
+                continue
+            case '*':
+                pathTree.push({
+                    type: 'always',
+                    value: '*'
+                })
+                continue
+            default:
+                staticPathLexer(char)
+                continue
+        }
+    }
+
+    return pathTree as PathTree
+}
--- src/router/prepared-router/router.ts
+++ src/router/prepared-router/router.ts
@@ -1,5 +1,7 @@
 import type { Params, Result, Router } from '../../router'
 import { checkOptionalParameter } from '../../utils/url'
+import { pathLexer } from './lexer'
+import type { PathTree } from './lexer'
 
 /*
 Generate Function
@@ -7,16 +9,20 @@ Generate Function
 
 export class PreparedRouter<T> implements Router<T> {
   name: string = 'PreparedRouter'
-  #preparedMatchFunction = new Function('method', 'path', 'return (()=>[])') as (
+  #preparedMatchFunction = new Function('method', 'path', 'return (()=>[])()') as (
     method: string,
     path: string
-  ) => [T, Params][]
-  routes: [string, string, T][] = []
-
-  constructor() {
+  ) => [number, Params][]
+  #routes: [string, [string, PathTree], T][] = []
+ 
+  constructor(init?: {
+    preparedMatchFunction: Function
+  }) {
     if (typeof Function === 'undefined') {
       throw new Error('This runtime does not support prepared router')
     }
+
+    Object.assign(this, init)
   }
 
   add(method: string, path: string, handler: T) {
@@ -24,16 +30,37 @@ export class PreparedRouter<T> implements Router<T> {
     if (optionalParameter) {
       optionalParameter.forEach((p) => this.add(method, p, handler))
     }else {
-      this.routes.push([method, path, handler])
+      this.#routes.push([method, [path, pathLexer(path)], handler])
       this.#buildPreparedMatchFunction()
     }
   }
 
   match(method: string, path: string): Result<T> {
-    return [this.#preparedMatchFunction(method, path)]
+    return [this.#preparedMatchFunction(method, path).map(([handlerIndex, params]) => [this.#routes[handlerIndex][2], params])]
   }
 
   #buildPreparedMatchFunction() {
+    const methodsWithRoutes: Record<string, [number, string, PathTree][]> = Object.create(null)
 
+    for (let i = 0, len = this.#routes.length; i < len; i++) {
+      const [method, [path, tree]] = this.#routes[i]
+
+      if (!methodsWithRoutes[method]) {
+        methodsWithRoutes[method] = []
+      }
+
+      methodsWithRoutes[method].push([i, path, tree])
+    }
+
+
+    this.#preparedMatchFunction = new Function(
+      'method',
+      'path',
+      `return (() => {
+        const matchResult = []
+
+        return matchResult
+      })()`
+    ) as (method: string, path: string) => [number, Params][]
   }
 }
