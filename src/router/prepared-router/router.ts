d140403c0fa76f1ab7b0a49434021bddbf4aa02f
yishideyun01 <yishideyun01@gmail.com>
2024-12-17 06:19:58 +0000

    fix

--- src/router/prepared-router/router.ts
+++ src/router/prepared-router/router.ts
@@ -201,25 +201,3 @@ export class PreparedRouter<T> implements Router<T> {
       })()`
   }
 }
-
-const routes = [
-  { method: 'GET', path: '/user' },
-  { method: 'GET', path: '/user/comments' },
-  { method: 'GET', path: '/user/avatar' },
-  { method: 'GET', path: '/user/lookup/username/:username' },
-  { method: 'GET', path: '/user/lookup/email/:address' },
-  { method: 'GET', path: '/event/:id' },
-  { method: 'GET', path: '/event/:id/comments' },
-  { method: 'POST', path: '/event/:id/comment' },
-  { method: 'GET', path: '/map/:location/events' },
-  { method: 'GET', path: '/status' },
-  { method: 'GET', path: '/very/deeply/nested/route/hello/there' },
-  { method: 'GET', path: '/static/*' },
-]
-
-const router = new PreparedRouter()
-for (const route of routes) {
-  router.add(route.method, route.path, `${route.method} ${route.path}`)
-}
-
-console.log(router.match('GET', '/event/123'))
